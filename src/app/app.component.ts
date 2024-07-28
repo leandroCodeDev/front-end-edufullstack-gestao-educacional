90045ecbfb4e9cc66f6646fb99c7dbabe25eec4b
Leandro Dias <lvd@certi.org.br>
2024-07-28 01:34:03 -0300

    ajusatdo margin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/componentes/navbar/navbar.component';
+import { CardContadorComponent } from './shared/componentes/card-contador/card-contador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CardContadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
