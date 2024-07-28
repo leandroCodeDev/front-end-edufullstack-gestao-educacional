8cc826dfbccd8a5055d70f3b5d3a771df972698c
Leandro Dias <lvd@certi.org.br>
2024-07-28 02:36:22 -0300

    criado componente de usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/componentes/navbar/navbar.component';
-import { CardContadorComponent } from './shared/componentes/card-contador/card-contador.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CardContadorComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/componentes/card-user/card-user.component.ts
+++ src/app/shared/componentes/card-user/card-user.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PhonePipe } from '../../pipes/phone-pipe/phone.pipe';
+
 
 @Component({
   selector: 'app-card-user',
   standalone: true,
-  imports: [],
+  imports: [PhonePipe],
   templateUrl: './card-user.component.html',
   styleUrl: './card-user.component.scss'
 })
 export class CardUserComponent {
-
+  @Input() idade!:number;
+  @Input() nome!:string;
+  @Input() telefone!:string;
 }
