b9235d8e172191b4bff7169aa78a2c38d98f5175
Leandro Dias <lvd@certi.org.br>
2024-07-28 14:57:32 -0300

    criado o formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'docentes',
+        loadComponent: () => import("./page/docente/docente/docente.component").then(c => c.DocenteComponent),
+      },
+      {
+        path: '**',
+        redirectTo: '/'
+      }
+];
