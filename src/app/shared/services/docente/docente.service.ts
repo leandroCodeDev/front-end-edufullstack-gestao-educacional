0f30e4da26c3731bb348a717b1fc7db9a6191949
Leandro Dias <lvd@certi.org.br>
2024-08-03 23:07:10 -0300

    corrigito service de docente

--- src/app/shared/services/docente/docente.service.ts
+++ src/app/shared/services/docente/docente.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Docente } from '../../interfaces/docente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocenteService {
 
-  constructor() { }
+  url = 'http://localhost:3000/docentes';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getDocentes() {
+    return this.httpClient.get<Array<Docente>>(this.url);
+  }
+
+  checkloginUser(login: string,senha:string) {
+    return this.httpClient.get<Array<Docente>>(this.url + `?login=${login}&senha=${senha}`);
+  }
+
+  getDocente(id: string) {
+    return this.httpClient.get<Docente>(this.url + `/${id}`);
+  }
+
+  postDocente(docente: Docente) {
+    return this.httpClient.post<any>(this.url, docente);
+  }
+
+  putDocente(docente: Docente) {
+    return this.httpClient.put<any>(this.url + `/${docente.id}`, docente);
+  }
+
+  delete(id: string) {
+    return this.httpClient.delete<any>(this.url + `/${id}`);
+  }
 }
