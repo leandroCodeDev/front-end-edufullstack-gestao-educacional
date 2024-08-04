e20f6ee2f71cbecc6d70886a0f4e0004345848aa
Leandro Dias <lvd@certi.org.br>
2024-08-03 23:04:57 -0300

    criado Service de aluno

--- src/app/shared/services/aluno/aluno.service.ts
+++ src/app/shared/services/aluno/aluno.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Aluno } from '../../interfaces/aluno';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlunoService {
 
-  constructor() { }
+  url = 'http://localhost:3000/alunos';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getAlunos() {
+    return this.httpClient.get<Array<Aluno>>(this.url);
+  }
+
+  checkloginUser(login: string,senha:string) {
+    return this.httpClient.get<Array<Aluno>>(this.url + `?login=${login}&senha=${senha}`);
+  }
+
+  getAluno(id: string) {
+    return this.httpClient.get<Aluno>(this.url + `/${id}`);
+  }
+
+  postAluno(aluno: Aluno) {
+    return this.httpClient.post<any>(this.url, aluno);
+  }
+
+  putAluno(aluno: Aluno) {
+    return this.httpClient.put<any>(this.url + `/${aluno.id}`, aluno);
+  }
+
+  delete(id: string) {
+    return this.httpClient.delete<any>(this.url + `/${id}`);
+  }
 }
