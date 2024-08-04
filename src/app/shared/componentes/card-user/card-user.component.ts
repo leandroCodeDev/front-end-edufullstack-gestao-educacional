7d03dba2dcec4a3877f5c23e97453d5e72d9aa49
Leandro Dias <lvd@certi.org.br>
2024-08-04 03:24:32 -0300

    ajustado card

--- src/app/shared/componentes/card-user/card-user.component.ts
+++ src/app/shared/componentes/card-user/card-user.component.ts
@@ -1,16 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PhonePipe } from '../../pipes/phone-pipe/phone.pipe';
+import { IdadePipe } from '../../pipes/idade/idade.pipe';
 
 
 @Component({
   selector: 'app-card-user',
   standalone: true,
-  imports: [PhonePipe],
+  imports: [PhonePipe, IdadePipe],
   templateUrl: './card-user.component.html',
   styleUrl: './card-user.component.scss'
 })
 export class CardUserComponent {
-  @Input() idade!:number;
+  @Output() clickCustom = new EventEmitter();
+  @Input() idade!:string;
   @Input() nome!:string;
   @Input() telefone!:string;
+
+
+  clickBtn(){
+    this.clickCustom.emit()
+  }
 }
