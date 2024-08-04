e4186ed63f984759129813c238fa01addbfa1d0c
Leandro Dias <lvd@certi.org.br>
2024-08-04 18:49:55 -0300

    revisao 1

--- src/app/shared/componentes/navbar/navbar.component.ts
+++ src/app/shared/componentes/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { LoginStore } from '../../store/login/Login.store';
+import { usuario } from '../../interfaces/usuario';
 
 @Component({
   selector: 'app-navbar',
@@ -12,7 +14,13 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 export class NavbarComponent {
   menuTitulo:string = 'LABPCP'
   private offcanvasService = inject(NgbOffcanvas);
-  
+  usuarioLogado!:usuario
+
+  constructor(
+    private loginStore:LoginStore
+  ){
+    this.usuarioLogado = loginStore.get()
+  }
 
 	open() {
 		const offcanvasRef = this.offcanvasService.open(SidebarComponent);
--- src/app/shared/interfaces/usuario.ts
+++ src/app/shared/interfaces/usuario.ts
@@ -2,6 +2,7 @@ import { PerfilEnum } from "../enums/perfil/perfil-enum";
 
 export interface usuario {
     id: string;
+    nome:string;
     login:string;
     email:string;
     senha:string;
